0fe12af6b1e541ab4a51853ae62ca98cd23f150e
Michael Jones <michael@zaptic.com>
2018-03-23 14:20:51 +0000

    Code & package updates
    Further work including support for number and plural. Or first draft
    anyway.

--- src/Message.elm
+++ src/Message.elm
@@ -1,51 +1,158 @@
 module Message exposing (..)
 
-import Parser
-    exposing
-        ( Parser
-        , (|.)
-        , (|=)
-        , succeed
-        , symbol
-        , float
-        , ignore
-        , zeroOrMore
-        , oneOrMore
-        , run
-        , oneOf
-        , keep
-        , map
-        , repeat
-        )
 import Char
+import Parser exposing (..)
 
 
 type Message
     = Phrase (List Message)
+    | Plural Value (List PluralOption)
     | Str String
-    | Variable String
+    | Variable Value
+    | Number Value NumberDisplay
+
+
+type alias Value =
+    String
+
+
+type NumberDisplay
+    = PercentDisplay
+
+
+type PluralOption
+    = IndexedOption Int Message
+    | OtherOption Message
+
+
+
+-- Parsers
+
+
+str : Parser Message
+str =
+    succeed Str
+        |= keep oneOrMore text
+
+
+number : Parser Message
+number =
+    delayedCommitMap
+        (\a b -> a)
+        (succeed Number
+            |. symbol "{"
+            |= keep oneOrMore variableToken
+            |. symbol ","
+            |. spaces
+            |. keyword "number"
+            |. spaces
+            |. symbol ","
+            |. spaces
+            |= percent
+            |. spaces
+        )
+        (symbol "}")
+
+
+percent : Parser NumberDisplay
+percent =
+    map (\_ -> PercentDisplay) (keyword "percent")
+
+
+plural : Parser Message
+plural =
+    let
+        indexedOption =
+            delayedCommitMap
+                (\a b -> a)
+                (succeed IndexedOption
+                    |. symbol "="
+                    |= int
+                    |. symbol "{"
+                    |= lazy (\_ -> phrase)
+                )
+                (symbol "}")
+
+        otherOption =
+            delayedCommitMap
+                (\a b -> a)
+                (succeed OtherOption
+                    |. keyword "other"
+                    |. symbol "{"
+                    |= lazy (\_ -> phrase)
+                )
+                (symbol "}")
+
+        option =
+            oneOf
+                [ indexedOption
+                , otherOption
+                ]
+    in
+    delayedCommitMap
+        (\a b -> a)
+        (inContext "plural" <|
+            succeed Plural
+                |. symbol "{"
+                |. spaces
+                |= keep oneOrMore variableToken
+                |. spaces
+                |. symbol ","
+                |. spaces
+                |. keyword "plural"
+                |. spaces
+                |. symbol ","
+                |. spaces
+                |= repeat oneOrMore option
+                |. spaces
+        )
+        (symbol "}")
+
+
+variable : Parser Message
+variable =
+    delayedCommitMap
+        (\v other -> v)
+        (succeed Variable |. symbol "{" |= keep oneOrMore text)
+        (symbol "}")
 
 
 phrase : Parser Message
 phrase =
-    map Phrase <|
-        repeat oneOrMore
-            (oneOf
-                [ succeed Str
-                    |= keep oneOrMore text
-                , succeed Variable
-                    |. symbol "{"
-                    |= keep oneOrMore text
-                    |. symbol "}"
-                ]
-            )
+    inContext "phrase" <|
+        map Phrase <|
+            repeat oneOrMore
+                (oneOf
+                    [ str
+                    , number
+                    , variable
+                    , plural
+                    ]
+                )
+
+
+
+-- Helpers
 
 
 text : Char -> Bool
 text char =
+    (char /= '{')
+        && (char /= '}')
+
+
+
+-- Char.isLower char
+--     || Char.isUpper char
+--     || (char == ' ')
+--     || List.member char ['"']
+
+
+variableToken : Char -> Bool
+variableToken char =
     Char.isLower char
         || Char.isUpper char
-        || (char == ' ')
+        || (char == '_')
 
 
 spaces : Parser ()
@@ -56,3 +163,8 @@ spaces =
 parse : String -> Result Parser.Error Message
 parse string =
     run phrase string
+
+
+parseWith : Parser Message -> String -> Result Parser.Error Message
+parseWith parser string =
+    run parser string
--- tests/Main.elm
+++ tests/Main.elm
@@ -1,13 +1,138 @@
-port module Main exposing (..)
+module Main exposing (..)
 
-import Tests
-import Test.Runner.Node exposing (run, TestProgram)
-import Json.Encode exposing (Value)
-
-
-main : TestProgram
-main =
-    run emit Tests.all
+import Expect
+import Message exposing (..)
+import Test exposing (..)
 
 
-port emit : ( String, Value ) -> Cmd msg
+all : Test
+all =
+    describe "Parser"
+        [ describe "Str"
+            [ test "All lower case" <|
+                \() ->
+                    Expect.equal (parse "abc") <| Ok (Phrase [ Str "abc" ])
+            , test "Some lower case" <|
+                \() ->
+                    Expect.equal (parse "abcABC") <| Ok (Phrase [ Str "abcABC" ])
+            , test "Some spaces" <|
+                \() ->
+                    Expect.equal (parse "abc ABC") <| Ok (Phrase [ Str "abc ABC" ])
+            ]
+        , describe "Variable"
+            [ test "Basic variable" <|
+                \() ->
+                    Expect.equal (parse "abc {bob} ABC") <|
+                        Ok
+                            (Phrase
+                                [ Str "abc "
+                                , Variable "bob"
+                                , Str " ABC"
+                                ]
+                            )
+            , test "With starting variable" <|
+                \() ->
+                    Expect.equal (parse "{bob} ABC") <|
+                        Ok
+                            (Phrase
+                                [ Variable "bob"
+                                , Str " ABC"
+                                ]
+                            )
+            , test "With ending variable" <|
+                \() ->
+                    Expect.equal (parse "ABC {bob}") <|
+                        Ok
+                            (Phrase
+                                [ Str "ABC "
+                                , Variable "bob"
+                                ]
+                            )
+            , test "With variable in quotes" <|
+                \() ->
+                    Expect.equal (parse "ABC \"{bob}\" DEF") <|
+                        Ok
+                            (Phrase
+                                [ Str "ABC \""
+                                , Variable "bob"
+                                , Str "\" DEF"
+                                ]
+                            )
+            , test "With two variables" <|
+                \() ->
+                    Expect.equal (parse "abc {bob} something {jim} ABC") <|
+                        Ok
+                            (Phrase
+                                [ Str "abc "
+                                , Variable "bob"
+                                , Str " something "
+                                , Variable "jim"
+                                , Str " ABC"
+                                ]
+                            )
+            ]
+        , describe "Number"
+            [ test "pure example" <|
+                \() ->
+                    Expect.equal (parseWith number "{pctBlack, number, percent}") <|
+                        Ok (Number "pctBlack" PercentDisplay)
+            , test "example in phrase" <|
+                \() ->
+                    Expect.equal (parse "Almost {pctBlack, number, percent} of them are black.") <|
+                        Ok
+                            (Phrase
+                                [ Str "Almost "
+                                , Number "pctBlack" PercentDisplay
+                                , Str " of them are black."
+                                ]
+                            )
+            , test "example in phrase with variable" <|
+                \() ->
+                    Expect.equal (parse "Almost {value} {pctBlack, number, percent} of them are black.") <|
+                        Ok
+                            (Phrase
+                                [ Str "Almost "
+                                , Variable "value"
+                                , Str " "
+                                , Number "pctBlack" PercentDisplay
+                                , Str " of them are black."
+                                ]
+                            )
+            ]
+        , describe "Plural"
+            [ test "pure example" <|
+                \() ->
+                    let
+                        string =
+                            "{num_files, plural, "
+                                ++ "=0{a}"
+                                ++ "=1{b}"
+                                ++ "other{c}}"
+                    in
+                    Expect.equal (parseWith plural string) <|
+                        Ok <|
+                            Plural
+                                "num_files"
+                                [ IndexedOption 0 (Phrase [ Str "a" ])
+                                , IndexedOption 1 (Phrase [ Str "b" ])
+                                , OtherOption (Phrase [ Str "c" ])
+                                ]
+            , test "example with nested variable" <|
+                \() ->
+                    let
+                        string =
+                            "{num_files, plural, "
+                                ++ "=0{a {x} a}"
+                                ++ "=1{b {y}}"
+                                ++ "other{{z} c}}"
+                    in
+                    Expect.equal (parseWith plural string) <|
+                        Ok <|
+                            Plural
+                                "num_files"
+                                [ IndexedOption 0 (Phrase [ Str "a ", Variable "x", Str " a" ])
+                                , IndexedOption 1 (Phrase [ Str "b ", Variable "y" ])
+                                , OtherOption (Phrase [ Variable "z", Str " c" ])
+                                ]
+            ]
+        ]
